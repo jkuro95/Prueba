b64a434fc0b45ec174680b357e33ae99fa03de82
jkuro95 <jkuro85@gmail.com>
2020-12-09 22:16:14 -0500

    Ejercicios de clase

---
+++ clase 8/par_impar.py
@@ -0,0 +1,8 @@
+def par(x):
+    if x % 2 == 0:
+        print(str(x) + " es un numero par")
+    else:
+        print(str(x) + " es un numero impar")
+
+x = float(input())
+par(x)
---
+++ clase 8/valor_absoluto.py
@@ -0,0 +1,13 @@
+def valor_absoluto(x):
+    if x < 0: 
+        return x * -1
+    else:
+        return x
+
+x = float(input())
+
+
+b = (True & True)|False
+if b == False:
+    print("hola")
+
